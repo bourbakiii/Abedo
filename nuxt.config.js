631293045953d729b753787aabf7313c90ed0d90
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-04-19 18:22:04 +0300

    control point

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+      { name: 'viewport', content:"width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/abedo-logo.ico' }
