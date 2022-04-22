ed726b74fa5d0dffbc8a798ce09b1bd4d2241159
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-04-22 16:01:21 +0300

    Control point

--- pages/addresses.vue
+++ pages/addresses.vue
@@ -1,27 +1,254 @@
 <template>
   <div class="page addresses-page wrapper">
-      <div class="addresses-page__content content">
-        <Breadcrumbs class="addresses-page__content__breadcrumbs"/>
-          <PagesInformationAddresses class='addresses-page__content__addresses'/>
+    <div class="addresses-page__content content">
+      <Breadcrumbs class="addresses-page__content__breadcrumbs" />
+      <div class="addresses-page__content__addresses">
+        <h1 class="addresses-page__content__addresses__title title-normal">
+          Адреса доставки
+        </h1>
+        <div class="addresses-page__content__addresses__content">
+          <div
+            v-for="(item, index) in 3"
+            :key="index"
+            class="addresses-page__content__addresses__content__item"
+          >
+            <p
+              class="addresses-page__content__addresses__content__item__name"
+              contenteditable=""
+            >
+              Работа
+            </p>
+            <div
+              class="addresses-page__content__addresses__content__item__data"
+            >
+              <InputBlock
+                id="dadata"
+                name="dadata"
+                class="addresses-page__content__addresses__content__item__data__input-block addresses-page__content__addresses__content__item__data__input-block__dadata"
+                text="Адрес доставки"
+              />
+              <div
+                class="addresses-page__content__addresses__content__item__data__additional"
+              >
+                <InputBlock
+                  id="intercom"
+                  name="intercom"
+                  class="addresses-page__content__addresses__content__item__data__additional__input-block addresses-page__content__addresses__content__item__data__additional__input-block__intercom"
+                  text="№ домофона"
+                />
+                <InputBlock
+                  id="entrance"
+                  name="entrance"
+                  class="addresses-page__content__addresses__content__item__data__additional__input-block addresses-page__content__addresses__content__item__data__additional__input-block__entrance"
+                  text="Подъезд"
+                />
+                <InputBlock
+                  id="floor"
+                  name="floor"
+                  class="addresses-page__content__addresses__content__item__data__additional__input-block addresses-page__content__addresses__content__item__data__additional__input-block__floor"
+                  text="Этаж"
+                />
+                <InputBlock
+                  id="apartment"
+                  name="apartment"
+                  class="addresses-page__content__addresses__content__item__data__additional__input-block addresses-page__content__addresses__content__item__data__additional__input-block__apartment"
+                  text="№ квартиры/офиса"
+                />
+              </div>
+              <ButtonStandart
+                class="addresses-page__content__addresses__content__item__data__button"
+                >Изменить</ButtonStandart
+              >
+            </div>
+          </div>
+        </div>
+        <ButtonStandart class="addresses-page__content__addresses__add">
+          <svg
+            class="addresses-page__content__addresses__add__icon"
+            width="7"
+            height="8"
+            viewBox="0 0 7 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M6.40481 3.86664H4.08104V1.54287C4.08104 1.22219 3.82136 0.961914 3.50009 0.961914C3.17882 0.961914 2.91913 1.22219 2.91913 1.54287V3.86664H0.595361C0.274092 3.86664 0.0144043 4.12691 0.0144043 4.4476C0.0144043 4.76828 0.274092 5.02855 0.595361 5.02855H2.91913V7.35232C2.91913 7.67301 3.17882 7.93328 3.50009 7.93328C3.82136 7.93328 4.08104 7.67301 4.08104 7.35232V5.02855H6.40481C6.72608 5.02855 6.98577 4.76828 6.98577 4.4476C6.98577 4.12691 6.72608 3.86664 6.40481 3.86664Z"
+              fill="#5C6784"
+            />
+          </svg>
+          Добавить адрес
+        </ButtonStandart>
       </div>
+    </div>
   </div>
 </template>
-
-<script>
-export default {
-
-}
-</script>
-
 <style lang="scss" scoped>
-.addresses-page{
+.addresses-page {
   padding-bottom: 100px;
-  
-  &__content{
+
+  &__content {
     flex-grow: 1;
-    &__breadcrumbs{
-      margin-top:20px;
-      margin-bottom:40px;
+    &__breadcrumbs {
+      margin-top: 20px;
+      margin-bottom: 40px;
+    }
+    &__addresses {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      flex-direction: column;
+      overflow: hidden;
+      max-width: 100%;
+      width: 100%;
+      &__title {
+        align-self: flex-start;
+        margin-bottom: 70px;
+      }
+      &__content {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+        flex-direction: column;
+        overflow: hidden;
+        max-width: 100%;
+        width: 100%;
+        border: 1px $dark_grey;
+        &__item {
+          overflow: hidden;
+          max-width: 100%;
+          width: 100%;
+          min-height: 185px;
+          padding: 30px 30px 40px;
+          background-color: $white;
+          border-radius: 20px;
+          display: flex;
+          align-items: flex-start;
+          justify-content: flex-start;
+          flex-direction: column;
+          margin-bottom: 30px;
+           @media screen and (max-width: $tablet_middle) {
+             padding: 20px;
+           }
+          &:last-of-type {
+            margin-bottom: 0px;
+          }
+          &__name {
+            font-family: "SF Pro Display";
+            font-style: normal;
+            font-weight: 400;
+            font-size: 22px;
+            line-height: 26px;
+            width: 100%;
+            margin-bottom: 28px;
+          }
+          &__data {
+            width: 100%;
+            display: flex;
+            align-items: flex-end;
+            justify-content: space-between;
+            flex-direction: row;
+            flex-wrap: nowrap;
+            @media screen and (max-width: $notebook) {
+              flex-wrap: wrap;
+            }
+            &__additional {
+              overflow: hidden;
+              width: 100%;
+              max-width: 100%;
+              margin-left: 30px;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              flex-direction: row;
+              @media screen and (max-width: $maxwidth) {
+                margin-left: 10px;
+              }
+              @media screen and (max-width: $notebook) {
+                margin-top: 15px;
+              }
+              @media screen and (max-width: $tablet_middle) {
+                flex-wrap: wrap;
+              }
+
+              &__input-block {
+                margin-right: 30px;
+                @media screen and (max-width: $maxwidth) {
+                  margin-right: 10px;
+                }
+                @media screen and (max-width: $notebook) {
+                  flex-grow: 1;
+                }
+                @media screen and (max-width: $tablet_middle) {
+                  width: calc(50% - 7.5px) !important;
+                   &:nth-of-type(1), &:nth-of-type(2){
+                     margin-bottom: 15px;
+                   }
+                  &:nth-of-type(1), &:nth-of-type(3){
+                    margin-right: 15px;
+                  }
+                  &:nth-of-type(2), &:nth-of-type(4){
+                    margin-right:0px;
+                  }
+                }
+                &:last-of-type {
+                  margin-right: 0px;
+                }
+                &__apartment {
+                  width: 142px;
+                  flex-shrink: 0;
+                }
+              }
+            }
+            &__input-block {
+              flex-grow: 1;
+              &__dadata {
+                @media screen and (max-width: $maxwidth) {
+                  width: 380px;
+                }
+                @media screen and (max-width: $notebook) {
+                  width: auto;
+                  order: -2;
+                }
+                width: 471px;
+                flex-shrink: 0;
+                flex-grow: 1;
+              }
+              &:last-of-type {
+                margin-right: 0px;
+              }
+            }
+            &__button {
+              @media screen and (max-width: $maxwidth) {
+                margin-left: 10px;
+              }
+              @media screen and (max-width: $notebook) {
+                order: -1;
+              }
+               @media screen and (max-width: $tablet_middle) {
+                 margin-left: 15px;
+               }
+              margin-left: 75px;
+              width: 102px;
+              flex-shrink: 0;
+            }
+          }
+        }
+      }
+      &__add {
+        margin-top: 40px;
+        width: 245px;
+        @media screen and (max-width: $maxwidth) {
+        }
+        &__icon {
+          margin-right: 10px;
+          width: 19px;
+          height: 19px;
+          border-radius: 90px;
+          padding: 2px;
+          border: 2px solid $darkblue;
+        }
+      }
     }
   }
 }
