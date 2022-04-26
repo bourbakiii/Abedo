383790bd655dc16461a3ef18cffb6221e75f255d
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-04-26 18:45:30 +0300

    Control point

---
+++ components/Pages/Order/Content.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="order-content">
+    <h2 class="order-content__title title-extra-normal">Заказанные позиции</h2>
+    <div class="order-content__content">
+      <div class="order-content__content__item">
+        <img
+          src="@/assets/images/product-placeholder.png"
+          class="order-content__content__item__image"
+        />
+        <p class="order-content__content__item__name" contenteditable>
+          Сырное ассорти
+        </p>
+        <div class="order-content__content__item__data">
+          <p class="order-content__content__item__data__count" contenteditable>
+            10
+          </p>
+          <p class="order-content__content__item__data__price" contenteditable>
+            350 ₽
+          </p>
+          <p
+            class="order-content__content__item__data__price order-content__content__item__data__price_full"
+            contenteditable
+          >
+            3 500 ₽
+          </p>
+        </div>
+      </div>
+      <div class="order-content__content__item">
+        <img
+          src="@/assets/images/product-placeholder.png"
+          class="order-content__content__item__image"
+        />
+        <p class="order-content__content__item__name" contenteditable>
+          Сырное ассорти
+        </p>
+        <div class="order-content__content__item__data">
+          <p class="order-content__content__item__data__count" contenteditable>
+            10
+          </p>
+          <p class="order-content__content__item__data__price" contenteditable>
+            350 ₽
+          </p>
+          <p
+            class="order-content__content__item__data__price order-content__content__item__data__price_full"
+            contenteditable
+          >
+            3 500 ₽
+          </p>
+        </div>
+      </div>
+      <div class="order-content__content__item">
+        <img
+          src="@/assets/images/product-placeholder.png"
+          class="order-content__content__item__image"
+        />
+        <p class="order-content__content__item__name" contenteditable>
+          Сырное ассорти
+        </p>
+        <div class="order-content__content__item__data">
+          <p class="order-content__content__item__data__count" contenteditable>
+            10
+          </p>
+          <p class="order-content__content__item__data__price" contenteditable>
+            350 ₽
+          </p>
+          <p
+            class="order-content__content__item__data__price order-content__content__item__data__price_full"
+            contenteditable
+          >
+            3 500 ₽
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.order-content {
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  &__title {
+    margin-bottom: 40px;
+    @media screen and (max-width: $tablet) {
+      font-family: "SF Pro Display" !important;
+      font-style: normal !important;
+      font-weight: 700 !important;
+      font-size: 20px !important;
+      line-height: 24px !important;
+      margin-bottom: 20px !important;
+    }
+  }
+  &__content {
+    width: 100%;
+    background-color: $white;
+    padding: 10px 30px;
+    border-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    flex-direction: column;
+    &__item {
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: flex-start;
+      flex-direction: row;
+      min-height: 106px;
+      background: $white;
+      border-bottom: 1px dashed $extra_dark_grey;
+      &:last-of-type {
+        border: none;
+      }
+      &__image {
+        width: 100px;
+        height: auto;
+        margin-right: 30px;
+        background-color: $white;
+        border-radius: 10px;
+        object-fit: contain;
+      }
+      &__name {
+        font-family: "SF Pro Display";
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 20px;
+        margin-right: 30px;
+        width: 100%;
+        max-width: 380px;
+      }
+
+      &__data {
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        flex-direction: row;
+
+        &__count {
+          font-family: "SF Pro Display";
+          font-style: normal;
+          font-weight: 500;
+          font-size: 20px;
+          line-height: 24px;
+          text-align: center;
+          width: 100%;
+          text-align: left;
+        }
+        &__price {
+          font-family: "Montserrat";
+          font-style: normal;
+          font-weight: 500;
+          font-size: 20px;
+          line-height: 24px;
+          white-space: nowrap;
+          text-align: center;
+          width: 100%;
+          margin: 0px 30px;
+          &_full {
+            margin: 0px;
+            text-align: right;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,65 @@
 <template>
-  <div class="page index-page wrapper">
-    <div class="index-page__content content">
-      <PagesIndexSearch class="index-page__content__search adaptive-non" />
-      <PagesIndexAdaptiveCategories
-        class="index-page__content__adaptive-categories adaptive"
-      />
-      <PagesIndexStocksSlider class="index-page__content__stocks-slider" />
-      <PagesIndexNewShops class="index-page__content__new-shops adaptive-non" />
-      <PagesIndexAdaptiveNewShops
-        class="index-page__content__adaptive-new-shops adaptive"
-      />
-      <PagesIndexAbout class="index-page__content__about adaptive-non">
-        Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру сгенерировать
-        несколько абзацев более менее осмысленного текста рыбы на русском языке,
-        а начинающему оратору отточить навык публичных выступлений в домашних
-        условиях. При создании генератора мы использовали небезизвестный
-        универсальный код речей. Текст генерируется абзацами случайным образом
-        от двух до десяти предложений в абзаце, что позволяет сделать текст
-        более привлекательным и живым для визуально-слухового восприятия.
-        <br />
-        По своей сути рыбатекст является альтернативой традиционному lorem
-        ipsum, который вызывает у некторых людей недоумение при попытках
-        прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском
-        языке наполнит любой макет непонятным смыслом и придаст неповторимый
-        колорит советских времен.
-      </PagesIndexAbout>
+  <div class="page index-page__all-wrapper">
+    <PagesIndexSearch class="index-page__content__search adaptive-non" />
+    <div class="index-page__wrapper wrapper">
+      <div class="index-page__content content">
+        <PagesIndexAdaptiveCategories
+          class="index-page__content__adaptive-categories adaptive"
+        />
+        <PagesIndexStocksSlider class="index-page__content__stocks-slider" />
+        <PagesIndexNewShops
+          class="index-page__content__new-shops adaptive-non"
+        />
+        <PagesIndexAdaptiveNewShops
+          class="index-page__content__adaptive-new-shops adaptive"
+        />
+        <PagesIndexAbout class="index-page__content__about adaptive-non">
+          Сайт рыбатекст поможет дизайнеру, верстальщику, вебмастеру
+          сгенерировать несколько абзацев более менее осмысленного текста рыбы
+          на русском языке, а начинающему оратору отточить навык публичных
+          выступлений в домашних условиях. При создании генератора мы
+          использовали небезизвестный универсальный код речей. Текст
+          генерируется абзацами случайным образом от двух до десяти предложений
+          в абзаце, что позволяет сделать текст более привлекательным и живым
+          для визуально-слухового восприятия.
+          <br />
+          По своей сути рыбатекст является альтернативой традиционному lorem
+          ipsum, который вызывает у некторых людей недоумение при попытках
+          прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском
+          языке наполнит любой макет непонятным смыслом и придаст неповторимый
+          колорит советских времен.
+        </PagesIndexAbout>
+      </div>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .index-page {
+  background-color: red;
   padding-bottom: 0px !important;
   @media screen and (max-width: $tablet) {
     padding-bottom: 50px !important;
   }
   &__content {
     &__search {
+      margin-bottom: 70px;
+    }
+
+    &__stocks-slider,
+    &__new-shops {
+      margin-bottom: 70px;
+    }
+    &__about {
       width: 100vw;
-      margin-bottom: 70px;
-    }
-    
-    &__stocks-slider, &__new-shops{
-      margin-bottom: 70px;
-    }
-    &__about{
-      width:100vw;
     }
     @media screen and (max-width: $tablet) {
-    &__adaptive-categories{
-      margin:30px 0px;
-    }  
-     &__stocks-slider{
-       margin-bottom: 60px;
-     }
+      &__adaptive-categories {
+        margin: 0px 0px 30px;
+      }
+      &__stocks-slider {
+        margin-bottom: 60px;
+      }
     }
   }
   @media screen and (max-width: $tablet) {
