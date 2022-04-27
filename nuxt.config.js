fe0ddee3feb3235f18b6b0ea9da86e2c109c33e9
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-04-27 14:05:22 +0300

    control point

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,8 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1, minimum-scale=1, user-scalable=no' },
+      // , minimum-scale=1, user-scalable=no
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
