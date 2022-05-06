5d42227630543462f610119e5d7de8f36c6285f6
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-05-06 22:52:52 +0300

    Start addresses

--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="page test-page wrapper">
-    <div class="test-page__content content">
-      <PagesPartnerStocksSlider class='adaptive'/>
-    </div>
+    <input type="text" v-model="query" @input="get_addresses_handler" />
+    {{suggestions}}
   </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      ya: [],
-      payment_type: "",
-    };
+  data(){
+    return{
+      suggestions:[],
+      timer: null,
+      query: null
+    }
   },
+  methods:{
+    get_addresses(){
+      this.$axios
+      .post(
+        "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address",
+        { query: this.query }, // data
+        {
+          headers: {
+            contentType: "application/json",
+            Authorization: "Token 86b3d2232cfec3a06b3f04020518fdbc5a7b7adf", // тут должен быть ваш токен
+          },
+        }
+      )
+      .then(({data:{suggestions}}) =>{
+        this.suggestions = suggestions;
+      });
+    },
+    get_addresses_handler(){
+      clearTimeout(this.timer);
+      this.timer = setTimeout(()=>{
+              clearTimeout(this.timer);
+              this.get_addresses();
+      },500);
+    }
+  }
 };
 </script>
 
---
+++ plugins/dadata.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import DadataSuggestions from 'vue-dadata-suggestions'
+
+Vue.use(DadataSuggestions,{
+  token: '86b3d2232cfec3a06b3f04020518fdbc5a7b7adf',
+  type: 'ADDRESS'
+});
