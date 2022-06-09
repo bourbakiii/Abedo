0b7539e659702d93acf9d08f578e293c547b1d14
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-09 19:13:47 +0300

    statics only get and set without redirects, cause back is going to change

--- store/index.js
+++ store/index.js
@@ -1,14 +1,29 @@
-export const actions = {
-    async nuxtServerInit({ state, dispatch, commit }, { req }) {
-        commit("account/actionWithoutLocalStorage", (state)=>{
-            state.token = this.$cookies.get('token')??null;
-        });
-
-    },
-    clientHook({ state, dispatch, commit }) {
-        commit("cart/local_set");
-        commit("account/local_set");
-        commit("variables/local_set");
-        if (state.account.token) dispatch("account/get");
+export default {
+  state() {
+    return {
+      pages: []
     }
+  }, mutations: {
+    action(state, action) {
+      action(state);
+    }
+  }, actions: {
+    async nuxtServerInit({state, dispatch, commit}, {req}) {
+      commit("account/actionWithoutLocalStorage", (state) => {
+        state.token = this.$cookies.get('token') ?? null;
+      });
+
+    }, clientHook({state, dispatch, commit}) {
+      commit("cart/local_set");
+      commit("account/local_set");
+      commit("variables/local_set");
+      if (state.account.token) dispatch("account/get");
+
+      this.$axios('/api/statics/pages').then(({data: {pages}}) => {
+        this.commit('action', (state)=>state.pages = pages);
+      })
+
+
+    }
+  }
 }
