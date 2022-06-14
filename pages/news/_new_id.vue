5afeca479e0e24bc3c71fa760b7e7f48f508a783
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-14 19:03:24 +0300

    News 90%

--- pages/news/_new_id.vue
+++ pages/news/_new_id.vue
@@ -3,13 +3,13 @@
     <div class="new-page__content content">
       <Breadcrumbs class="new-page__content__breadcrumbs adaptive-non"/>
       <div class="new-page__content__content">
-        <h1 class="new-page__content__content__title title-normal">{{ item.title }}</h1>
+        <h1 class="new-page__content__content__title title-extra-normal">{{ item.title }}</h1>
         <img class="new-page__content__content__image" :src="`${$axios.defaults.baseURL}${item.image.original}`"
              v-if="item.image" alt="Item image"/>
-        <!--      <div v-html="item.content" class="new-page__content__content">-->
+        <div v-html="item.content" class="new-page__content__content__text">
+        </div>
       </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -43,38 +43,50 @@ export default {
   justify-content: flex-start;
 
   &__content {
-
-    &__title {
-      margin-bottom: 70px;
-      align-self: flex-start;
-      @media screen and (max-width: $tablet) {
-        margin-bottom: 40px;
-        font-family: "SF Pro Display";
-        font-style: normal;
-        font-weight: 700;
-        font-size: 20px;
-        line-height: 20px;
-        margin-top: 20px;
-      }
-    }
-
     &__content {
-      text-align: left;
-      align-self: flex-start;
-      font-family: "SF Pro Display";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 24px;
-      @media screen and (max-width: $tablet) {
-        font-size: 15px;
+      max-width: 835px;
+
+      &__title {
+        margin-bottom: 36px;
+        align-self: flex-start;
+        @media screen and (max-width: $tablet) {
+          font-family: 'SF Pro Display';
+          font-style: normal;
+          font-weight: 700;
+          font-size: 20px;
+          line-height: 26px;
+          margin-bottom: 20px;
+        }
       }
 
-      &__content {
-        max-width: 835px;
+      &__image {
+        max-width: 100%;
+        margin-bottom: 40px;
+        object-fit: contain;
+        @media screen and (max-width: $tablet) {
+          margin-bottom: 20px;
+        }
+      }
+
+      &__text {
+        font-family: 'SF Pro Display';
+        font-style: normal;
+        font-weight: 500;
+        font-size: 16px;
+        line-height: 26px;
+        @media screen and (max-width: $tablet) {
+          font-family: 'SF Pro Display';
+          font-style: normal;
+          font-weight: 400;
+          font-size: 15px;
+          line-height: 21px;
+        }
       }
     }
+
+
   }
+
 }
 </style>
 
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,23 +1,21 @@
 <template>
-  <div class="page news-page wrapper">
-    <div class="news-page__content content">
-      <Breadcrumbs class="news-page__content__breadcrumbs adaptive-non"/>
-      <h1 class="news-page__content__title title-normal">Новости</h1>
-      <div class="news-page__content__content" v-if="news.length">
-        <NuxtLink :to="`/news/${item.id}`" class="news-page__content__content__item" v-for="item in news"
-                  :key="item.id">
-          <p class="news-page__content__content__item__date">
-            {{ parseDate(item.created_at, {with_time: false}) }}
-          </p>
-          <h3 class="news-page__content__content__item__title">{{ item.title }}</h3>
+  <div class="page news-page__wrapper wrapper">
+    <div class="news-page content">
+      <Breadcrumbs class="news-page__breadcrumbs adaptive-non"/>
+      <h1 class="news-page__title adaptive-non">Новости</h1>
+      <div class="news-page__content">
+        <NuxtLink class="news-page__content__link" :to="`/news/${item.id}`" v-for="item in news" :key='item.id'>
+        <span class="news-page__content__link__date">
+          {{ parseDate(item.created_at, {with_time: false}) }}
+        </span>
+          <h3 class="news-page__content__link__title">{{ item.title }}</h3>
         </NuxtLink>
+
       </div>
-      <p class="news-page__content__content_empty" v-else>
-        Кажется, новостей пока нет
-      </p>
     </div>
   </div>
 </template>
+
 
 <script>
 import parser from "@/mixins/parser";
@@ -37,91 +35,87 @@ export default {
   },
   async fetch() {
     this.loading = true;
-    await this.$axios.get("/api/news").then(({data: {news}}) => {
-      console.log("йа мады");
-      console.log(news);
-      this.news = news.data;
-    }).finally(() => this.loading = false);
+    await this.$axios.get("/api/news").then(({data: {news}}) => this.news = news.data).finally(() => this.loading = false);
   }
 }
 </script>
+
 <style lang="scss" scoped>
 .news-page {
-  justify-content: flex-start;
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  align-items: flex-start;
+
+  &__title {
+    margin-bottom: 40px;
+  }
 
   &__content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+    width: 100%;
 
-    &__title {
-      margin-bottom: 70px;
-      align-self: flex-start;
-      @media screen and (max-width: $tablet) {
-        margin-bottom: 40px;
-        font-family: "SF Pro Display";
-        font-style: normal;
-        font-weight: 700;
-        font-size: 20px;
-        line-height: 20px;
-        margin-top: 20px;
-      }
-    }
+    &__link {
+      text-decoration: none;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: flex-start;
+      margin-bottom: 40px;
 
-    &__content {
-      text-align: left;
-      align-self: flex-start;
-      font-family: "SF Pro Display";
-      font-style: normal;
-      font-weight: 400;
-      font-size: 16px;
-      line-height: 24px;
-      @media screen and (max-width: $tablet) {
-        font-size: 15px;
+      &:last-of-type {
+        margin-bottom: 0 !important;
       }
 
-      &__item {
-        text-decoration: none;
-        margin-bottom: 40px;
+      @media screen and (max-width: $tablet) {
+        margin-bottom: 15px;
+        background-color: $white;
+        width: 100%;
+        border-radius: 10px;
+        padding: 20px 14px;
+      }
 
-        &:last-of-type {
-          margin-bottom: 0px;
+      &__date {
+        margin-bottom: 10px;
+        font-family: 'SF Pro Display';
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 19px;
+        color: $darkblue;
+        @media screen and (max-width: $tablet) {
+          font-weight: 400;
+          font-size: 14px;
+          line-height: 17px;
         }
+      }
 
-        &__date {
-          margin-bottom: 15px;
-          font-family: 'SF Pro Display';
-          font-style: normal;
+      &__title {
+        font-family: 'Montserrat';
+        font-style: normal;
+        font-weight: 600;
+        font-size: 18px;
+        line-height: 34px;
+        @media screen and (max-width: $tablet) {
           font-weight: 400;
           font-size: 16px;
-          line-height: 19px;
-          color: $darkblue;
+          line-height: 24px;
         }
+      }
 
+      &:hover & {
         &__title {
-          font-family: 'Montserrat';
-          font-style: normal;
-          font-weight: 600;
-          font-size: 18px;
-          line-height: 25px;
-
-          &:hover {
-            text-decoration: underline;
-          }
+          text-decoration: underline;
         }
       }
 
-      &_empty {
-        text-align: left;
-        width: max-content;
-        font-family: 'SF Pro Display';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 16px;
-        line-height: 26px;
-        align-self: flex-start;
-      }
     }
   }
+
 }
 </style>
-
-
-
