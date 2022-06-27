49298d49c1602c0cd26c567b12678d6fea1153fb
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-27 16:00:04 +0300

    Sprites 1/10

--- components/Product/Cart/Adaptive.vue
+++ components/Product/Cart/Adaptive.vue
@@ -1,18 +1,14 @@
 <template>
   <div class="product-cart-adaptive product">
     <button @click="remove" class="product-cart-adaptive__close">
+
+
       <svg
         class="sidebar-cart__products__item__close__icon"
         width="15"
         height="15"
-        viewBox="0 0 20 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
       >
-        <path
-          d="M8.89533 10.0005L0.229061 1.33421C-0.0760555 1.02909 -0.0760555 0.534404 0.229061 0.229326C0.534178 -0.0757519 1.02886 -0.075791 1.33394 0.229326L10.0003 8.89565L18.6665 0.229326C18.9716 -0.075791 19.4663 -0.075791 19.7714 0.229326C20.0765 0.534443 20.0765 1.02913 19.7714 1.33421L11.1051 10.0005L19.7714 18.6668C20.0765 18.9719 20.0765 19.4666 19.7714 19.7717C19.6189 19.9242 19.4189 20.0005 19.2189 20.0005C19.019 20.0005 18.8191 19.9242 18.6665 19.7717L10.0003 11.1054L1.33398 19.7717C1.18144 19.9242 0.981482 20.0005 0.781521 20.0005C0.58156 20.0005 0.381639 19.9242 0.229061 19.7717C-0.0760555 19.4666 -0.0760555 18.9719 0.229061 18.6668L8.89533 10.0005Z"
-          fill="#A6A8A8"
-        />
+        <use xlink:href="@/assets/sprites/sprites.svg#modal-close"></use>
       </svg>
     </button>
     <p class="product-cart-adaptive__name">
@@ -20,7 +16,7 @@
     </p>
     <div class="product-cart-adaptive__data">
       <NuxtLink
-          v-if="product.image"
+        v-if="product.image"
         :to="`/product/${product.id}`"
         class="product-cart-adaptive__data__image"
       >
@@ -50,7 +46,8 @@
         />
       </div>
       <div class="product-cart-adaptive__data__prices">
-        <p class="product-cart-adaptive__data__prices__price" :class="{'product-cart-adaptive__data__prices__price_red': discount_percent}">
+        <p class="product-cart-adaptive__data__prices__price"
+           :class="{'product-cart-adaptive__data__prices__price_red': discount_percent}">
           {{ product_price_with_discount }}₽
         </p>
         <p
@@ -123,6 +120,7 @@
 </template>
 <script>
 import productsMixin from "@/mixins/product.js";
+
 export default {
   mixins: [productsMixin],
   props: {
@@ -147,6 +145,7 @@ export default {
   flex-direction: column;
   width: 100%;
   position: relative;
+
   &__close {
     background-color: transparent;
     border: none;
@@ -156,6 +155,7 @@ export default {
     top: 14px;
     right: 14px;
   }
+
   &__name {
     font-family: "SF Pro Display";
     font-style: normal;
@@ -166,12 +166,14 @@ export default {
     text-align: left;
     width: 100%;
   }
+
   &__data {
     width: 100%;
     display: flex;
     align-items: center;
     justify-content: flex-start;
     flex-direction: row;
+
     &__image {
       width: 30px;
       height: 30px;
@@ -179,17 +181,20 @@ export default {
       margin-right: 20px;
       overflow: hidden;
       position: relative;
+
       &__image {
         width: 100%;
         height: 100%;
         object-fit: cover;
       }
     }
+
     &__buttons {
       display: flex;
       align-items: center;
       justify-content: center;
       margin-right: 20px;
+
       &__count {
         width: 40px;
         max-width: 40px;
@@ -202,6 +207,7 @@ export default {
         line-height: 19px;
       }
     }
+
     &__prices {
       display: flex;
       align-items: flex-end;
@@ -209,15 +215,18 @@ export default {
       flex-direction: column-reverse;
       justify-self: flex-end;
       margin-left: auto;
+
       &__price {
         font-family: "SF Pro Display";
         font-style: normal;
         font-weight: 500;
         font-size: 15px;
         line-height: 16px;
-        &_red{
-          color:$red;
+
+        &_red {
+          color: $red;
         }
+
         &_full {
           font-size: 13px;
           line-height: 15px;
@@ -225,17 +234,19 @@ export default {
           text-decoration: line-through;
           margin-left: 10px;
           font-family: 'SF Pro Display';
-font-style: normal;
-font-weight: 500;
-font-size: 14px;
-line-height: 16px;
+          font-style: normal;
+          font-weight: 500;
+          font-size: 14px;
+          line-height: 16px;
         }
       }
     }
   }
+
   &:last-of-type {
     margin-bottom: 0;
   }
+
   &__options {
     display: flex;
     align-items: flex-end;
@@ -243,6 +254,7 @@ line-height: 16px;
     flex-direction: column;
     width: 100%;
     margin-top: 15px;
+
     &__content {
       display: flex;
       align-items: center;
@@ -253,6 +265,7 @@ line-height: 16px;
       border-top: 1px solid $dark_grey;
       border-bottom: 1px solid $dark_grey;
       margin-bottom: 14px;
+
       &__item {
         width: 100%;
         margin-bottom: 5px;
@@ -260,12 +273,15 @@ line-height: 16px;
         align-items: center;
         justify-content: flex-start;
         flex-direction: row;
+
         &:last-of-type {
           margin-bottom: 0;
         }
+
         &__icon {
           flex-shrink: 0;
         }
+
         &__name {
           font-family: "SF Pro Display";
           font-style: normal;
@@ -274,11 +290,13 @@ line-height: 16px;
           line-height: 17px;
           margin: 0 0 0 12px;
         }
+
         &__dots {
           flex-grow: 1;
           border-bottom: 1px dotted $dark_grey;
           margin: 0 6px;
         }
+
         &__price {
           flex-shrink: 0;
           font-family: "SF Pro Display";
@@ -291,12 +309,14 @@ line-height: 16px;
           align-items: center;
           justify-content: flex-end;
           flex-direction: row;
+
           &__price {
             color: $black;
           }
         }
       }
     }
+
     &__total {
       font-family: "SF Pro Display";
       font-style: normal;
@@ -307,6 +327,7 @@ line-height: 16px;
       align-items: center;
       justify-content: flex-end;
       flex-direction: row;
+
       &__price {
         font-family: "SF Pro Display";
         font-style: normal;
@@ -314,6 +335,7 @@ line-height: 16px;
         font-size: 15px;
         line-height: 16px;
         margin-left: 10px;
+
         &_full {
           margin-left: 10px;
           color: $extra_dark_grey;
