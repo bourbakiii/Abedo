d2a20557a0eef1ea2a1474eee3d1b5e03d4f768b
bourbakiii <55923749+bourbakiii@users.noreply.github.com>
2022-06-27 12:54:46 +0300

    App reminder timer terms

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,10 +15,8 @@ export default {
   mounted() {
     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
       const last_dateTime = this.$cookies.get("app_remind_last_show_time") ? new Date(this.$cookies.get("app_remind_last_show_time")) : null;
-
-
       if (!last_dateTime || new Date() > last_dateTime) {
-        this.$cookies.set('app_remind_last_show_time', new Date((new Date().setHours(24))));
+        this.$cookies.set('app_remind_last_show_time', new Date(Date.now() + 1000 * 60 * 60 * 24));
         this.$store.commit('modals/open', {modal_name: 'app_remind'});
       }
     }
